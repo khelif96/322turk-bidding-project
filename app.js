7bfc5cecb9cda07153f1e27b113f0e768948a798
Mohamed Khelif <khelif96@gmail.com>
2017-10-08 01:57:59 -0400

    Began Configuration of Mongoose

--- app.js
+++ app.js
@@ -12,6 +12,36 @@ var hat = require('hat'); // Library for generating random ids
 // Winston Logger
 var logger = require('./utils/logger.js');
 
+
+// Mongodb Config
+mongoose.connect(process.env.DB_URL,{useMongoClient:true}); // Connect to database on Server
+
+var db = mongoose.connection;
+
+db.once('open', function() {
+  // we're connected!
+  logger.log("info", "Status Code " + mongoose.connection.readyState + " Connected");
+
+});
+
+// When the connection is disconnected
+db.on('disconnected', function () {
+  logger.log("info", 'Mongoose default connection disconnected');
+});
+
+db.on('error', function(){
+  logger.log("error", "ERROR Status Code " + mongoose.connection.readyState);
+});
+
+// If the Node process ends, close the Mongoose connection
+process.on('SIGINT', function() {
+  db.close(function () {
+    logger.log('info', 'Mongoose default connection disconnected through app termination');
+    process.exit(0);
+  });
+});
+
+
 logger.log('info', "Server Starting");
 
 var port = 3000;
