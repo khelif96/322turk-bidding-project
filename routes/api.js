3b6a22011fdd1181cc0f348fbec867b2abaffd09
Mohamed Khelif <khelif96@gmail.com>
2017-10-13 13:07:07 -0400

    implemented routes for login and register

--- routes/api.js
+++ routes/api.js
@@ -5,11 +5,16 @@ const express = require('express');
 const router = express.Router();
 
 const apiHome = require('../controller/apiHome');
-
+const userAuth = require('../controller/userAuth');
 
 // API
 router.get('/', apiHome.getApi);
 router.post('/', apiHome.postApi);
+
+router.get('/loginUser', userAuth.getLoginUser);
+router.post('/loginUser', userAuth.loginUser);
+
+router.post('/registerUser', userAuth.registerUser);
 
 // Return Router
 module.exports = router;
