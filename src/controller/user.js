d472e21723d34d31ba3a879ba6189f971f96debc
Mohamed Khelif <khelif96@gmail.com>
2017-12-01 21:05:57 -0500

    Middlewear for checking if user type is allowed to access a route

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,6 +1,37 @@
 var mongoose = require('mongoose'); // Interface for mongodb
 
 var User = require('../app/models/userSchema');
+
+exports.isDeveloper = (req,res,next) => {
+  User.findOne({api_token: req.body.api_token}, '-password_hash' ,function(err,userDoc){
+      if(!userDoc || err){
+          res.status(401).json({error: "Invalid api_token"});
+      }
+      else{
+        if(userDoc.userType === "Developer"){
+          next();
+        }else{
+          res.status(400).json({error: "You need to be a Developer to use this action"});
+        }
+      }
+  });
+}
+
+exports.isClient = (req,res,next) => {
+  User.findOne({api_token: req.body.api_token}, '-password_hash' ,function(err,userDoc){
+      if(!userDoc || err){
+          res.status(401).json({error: "Invalid api_token"});
+      }
+      else{
+        if(userDoc.userType === "Client"){
+          next();
+        }else{
+          res.status(400).json({error: "You need to be a Client to use this action"});
+        }
+      }
+  });
+}
+
 exports.getUserByApiToken = (req,res) => {
     User.findOne({api_token: req.params.api_token}, '-password_hash' ,function(err,userDoc){
         if(!userDoc || err){
