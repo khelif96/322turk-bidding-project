e2580dd1d25e4da53425a950c49f465ed080a7bf
Matthew <matthew.10.318@gmail.com>
2017-12-05 17:50:13 -0500

    Cahnged database scheme for users and demands. All demands and bids now require dates

--- src/routes/api.js
+++ src/routes/api.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 const apiHome = require('../controller/apiHome');
 const userAuth = require('../controller/userAuth');
+const userModification = require('../controller/userModification');
 
 const user = require('../controller/user');
 
@@ -33,13 +34,21 @@ router.use(userAuth.checkAuth); // Routes that require and api_token after this
 
 
 // Post Modification Routes //
+// Pass "api_token", "title", "content", "date"
 router.post('/createDemand', user.isClient, demandModification.createDemand);
 router.put('/editDemand/:demandId', user.isClient,demandModification.editDemand);
 
+// Pass "api_token", "demandId", "bidAmount", "deadline"
 router.post('/bidOnDemand', user.isDeveloper, demandModification.bidOnDemand);
 
+// Pass "api_token", "devId", "demandId", and "reason" only if chosen bidder is not the lowest
 router.post('/approveBidder', user.isClient, demandModification.approveBidder);
-router.post('/addFunds', user.isClient, userAuth.addFunds);
+
+// Pass "api_token", "demandId", and "finishedProduct"
+router.post('/submitProduct', user.isDeveloper, demandModification.submitProduct);
+
+// Pass "api_token" and "amount"
+router.post('/addFunds', user.isClient, userModification.addFunds);
 
 // Return Router
 module.exports = router;
