b3ee7df7dabda4e46b25cb2155a8a7faf41df574
Mohamed Khelif <khelif96@gmail.com>
2017-12-05 23:19:13 -0500

    Modified demand route to also return user profile info instead of jsut owner id

--- src/routes/api.js
+++ src/routes/api.js
@@ -27,6 +27,8 @@ router.post('/registerUser', userAuth.registerUser);
 
 router.get('/demands', demands.getAllDemands);
 router.get('/demands/:demandId', demands.getDemand);
+router.get('/demands/:demandId/:expanded', demands.getDemand);
+
 
 router.get('/user/api_token=:api_token', user.getUserByApiToken);
 
