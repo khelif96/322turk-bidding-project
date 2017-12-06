900b48fe9b13707e11513eabed826f0c606ed76e
Mohamed Khelif <khelif96@gmail.com>
2017-12-06 14:11:44 -0500

    Middlewear ot check if path is correct

--- src/controller/apiHome.js
+++ src/controller/apiHome.js
@@ -10,3 +10,10 @@ exports.postApi = (req,res) => {
   res.json({error: "Route does not allow POST"});
   res.status(200);
 };
+
+// Middle wear to check if path is correct
+exports.invalidPath = (req,res,next) => {
+
+    res.status(400).json({"error": "Invalid Path"});
+
+}
