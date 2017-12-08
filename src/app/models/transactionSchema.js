29f44f6a328dc0e09f1df5c1182757eb6d094d1f
Mohamed Khelif <khelif96@gmail.com>
2017-12-07 21:20:56 -0500

    Created a new Schema for transactions

---
+++ src/app/models/transactionSchema.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+// Schema
+var transactionSchema = new Schema({
+  createdDate : {type : Date, default:Date.now},
+  origin_id : {type: String, required: true}, // Who made the transaction
+  destination_id : String, // Is the transaction going to any one
+  amount : Number,
+  complete : {type:Boolean, default:false},
+  transactionType: String // 2 Types AddFunds, Payments
+
+});
+
+module.exports = mongoose.model('Transaction',transactionSchema);
